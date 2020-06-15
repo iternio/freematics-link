1fde349e4c064454c2bec8f5133eef42a29dd335
Billy Gallagher <bjg222@gmail.com>
2020-06-15 12:23:26 -0400

    Added ntp time synchronization to get actual time once connected to the internet, finished beep implementation, commented out HTTPClient implementation, using new implementation only (note, send functionality not yet implemented)

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #ifdef BOARD_HAS_PSRAM
 #include <esp_himem.h>
 #endif
-#include "httpclientwrapper.h"
+#include <lwip/apps/sntp.h>
+// #include "httpclientwrapper.h"
 
 // #include "mems.h"
 // #include "abrpwifi.h"
@@ -12,10 +13,12 @@
 #include "abrp.h"
 // #include "config.h"
 
-#define WIFI_SSID "-----"
-#define WIFI_PASSWORD "-----"
+#include "abrp/abrp.h"
+#include "config/config.h"
+#include "config/private.h"
 
-#define LOOP_TIME 10000
+#define WIFI_SSID "Black Hole"
+#define WIFI_PASSWORD "eventhorizon"
 
 FreematicsESP32 sys;
 
@@ -30,8 +33,7 @@ float round(float val, float dec = 1) {
 void blink(uint32_t ms = 50, byte n = 1)
 {
     static bool setup = false;
-    if (!setup)
-    {
+    if (!setup) {
         pinMode(PIN_LED, OUTPUT);
         setup = true;
     }
@@ -46,10 +48,34 @@ void blink(uint32_t ms = 50, byte n = 1)
 
 void beep(uint32_t freq = 2000, uint32_t ms = 50, byte n = 1)
 {
+    static bool setup = false;
+    if (!setup) {
+        ledcSetup(0, 2000, 8);
+        ledcAttachPin(PIN_BUZZER, 0);
+        setup = true;
+    }
+    ledcWriteTone(0, freq);
+    for (byte i = 0; i < n; i++) {
+        ledcWrite(0, 255);
+        delay(ms);
+        ledcWrite(0, 0);
+        if (i < n-1)
+            delay(ms);
+    }
 }
 
 void printSysInfo(const FreematicsESP32 &sys)
 {
+    esp_chip_info_t cinfo;
+    esp_chip_info(&cinfo);
+    log_i("Chip: %s", cinfo.model == CHIP_ESP32 ? "ESP32" : "Unknown");
+    log_i("Cores: %u", cinfo.cores);
+    log_i("Rev: %u", cinfo.revision);
+    log_i("Features: EMB %c, WiFi %c, BLE %c, BT %c",
+        cinfo.features & CHIP_FEATURE_EMB_FLASH ? 'Y' : 'N',
+        cinfo.features & CHIP_FEATURE_WIFI_BGN ? 'Y' : 'N',
+        cinfo.features & CHIP_FEATURE_BLE ? 'Y' : 'N',
+        cinfo.features & CHIP_FEATURE_BT ? 'Y' : 'N');
     log_i("CPU: %u MHz", ESP.getCpuFreqMHz());
     log_i("Flash: %u B", ESP.getFlashChipSize());
     // log_i("Flash: %u B", spi_flash_get_chip_size());
@@ -57,10 +83,11 @@ void printSysInfo(const FreematicsESP32 &sys)
     log_i("IRAM: %u B", ESP.getHeapSize());
     log_i("PSRAM: %u B", ESP.getPsramSize() + esp_himem_get_phys_size());
 #endif
+    //TODO: I'm pretty sure this board is supposed to have an RTC, figure this out
     int rtc = rtc_clk_slow_freq_get();
     if (rtc)
         log_i("RTC: %i", rtc);
-    log_i("Firmware: R%i", sys.version);
+    log_i("Firmware: R%i\n\n", sys.version);
 }
 
 void startWiFi() {
@@ -77,46 +104,83 @@ void startWiFi() {
             delay(2000);
         }
     }
-    log_v("Connected");
+    log_v("Connected\n\n");
 }
 
-MyHTTP::MyClient ABRPclient;
-
-void connectABRP() {
-    ABRPclient.begin(
-        "https://" ABRP_SERVER_HOST ABRP_SERVER_ENDPOINT "?"
-            ABRP_SERVER_TOKENVAR "=" ABRP_SERVER_TOKEN,
-        ABRP_SERVER_CACERT);
+void time_sync_notification_cb(struct timeval *tv)
+{
+    log_v("Notification of a time synchronization event");
 }
 
-void sendABRP(const String &tlm) {
-    String payload =
-        "--ABR1234PABRP1234ABRP\n"
-        "Content-Disposition: form-data; name=\"tlm\"\n\n"
-        + tlm + "\n"
-        "--ABR1234PABRP1234ABRP--";
-    ABRPclient.addHeader(ABRP_SERVER_AUTHHEADER, ABRP_SERVER_AUTHTEXT ABRP_SERVER_APIKEY);
-    ABRPclient.addHeader("Content-Type", "multipart/form-data; boundary=\"ABR1234PABRP1234ABRP\"");
-    ABRPclient.post(payload);
-    log_v("%s", "Sent");
-    WiFiClient stream = ABRPclient.getStream();
-    while (stream.available()) {
-        char ch = static_cast<char>(stream.read());
-        Serial.print(ch);
+void setTime() {
+    time_t now;
+    tm* nowtm;
+    char buf[35];
+    time(&now);
+    nowtm = gmtime(&now);
+    strftime(buf, 35, "%a, %d %b %Y %H:%M:%S GMT", nowtm);
+    log_v("Current system time: %s", buf);
+    log_v("Getting time via SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, (char*)"pool.ntp.org");
+    sntp_init();
+    delay(500);
+    while (time(nullptr) < (2020 - 1970) * 365 * 24 * 3600) {
+        log_v("Waiting");
+        delay(1000);
     }
-    Serial.println();
-    log_v("%s", "Done");
-    Serial.println();
-    Serial.println();
-    Serial.println();
+    // sntp_stop();
+    time(&now);
+    nowtm = gmtime(&now);
+    strftime(buf, 35, "%a, %d %b %Y %H:%M:%S GMT", nowtm);
+    log_v("System time updated to: %s\n\n", buf);
 }
+
+// MyHTTP::MyClient ABRPclient;
+
+// void connectABRP() {
+//     ABRPclient.begin(
+//         "https://" ABRP_SERVER_HOST ABRP_SERVER_ENDPOINT "?"
+//             ABRP_SERVER_TOKENVAR "=" ABRP_SERVER_TOKEN,
+//         ABRP_SERVER_CACERT);
+// }
+
+// void sendABRP(const String &tlm) {
+//     String payload =
+//         "--ABR1234PABRP1234ABRP\n"
+//         "Content-Disposition: form-data; name=\"tlm\"\n\n"
+//         + tlm + "\n"
+//         "--ABR1234PABRP1234ABRP--";
+//     ABRPclient.addHeader(ABRP_SERVER_AUTHHEADER, ABRP_SERVER_AUTHTEXT ABRP_SERVER_APIKEY);
+//     ABRPclient.addHeader("Content-Type", "multipart/form-data; boundary=\"ABR1234PABRP1234ABRP\"");
+//     ABRPclient.post(payload);
+//     log_v("%s", "Sent");
+//     WiFiClient stream = ABRPclient.getStream();
+//     while (stream.available()) {
+//         char ch = static_cast<char>(stream.read());
+//         Serial.print(ch);
+//     }
+//     Serial.println();
+//     log_v("%s", "Done");
+//     Serial.println();
+//     Serial.println();
+//     Serial.println();
+// }
 
 ABRPTelemetry telem;
+abrp::clients::HTTP c;
+WiFiClient w;
 
 void setup()
 {
+    log_v("Boot complete\n\n");
+    // sys.buzzer(2000);
+    // delay(200);
+    // sys.buzzer(0);
+    beep(880, 100);
+    delay(200);
+
     log_v("Beginning set up");
-    // delay(250);
     Serial.begin(115200);
     blink(25);
     if (sys.begin()) {
@@ -124,39 +188,57 @@ void setup()
         blink(25, 3);
         printSysInfo(sys);
     }
-    startWiFi();
-    // connectABRP();
-    // telem.utc = 1591385576l + millis() / 1000l;
-    // telem.soc = randfloat(0, 100, 1);
-    // telem.speed = randfloat(0, 160, 1);
-    // telem.lat = randfloat(-90, 90, 7);
-    // telem.lon = randfloat(-180, 180, 7);
-    // telem.is_charging = false;
-    // telem.soh = randfloat(0, 100, 1);
-    // sendABRP(telem.toJSON());
-    // Serial.println(telem.toJSON());
 
-    abrp::clients::HTTP c;
-    WiFiClient w;
-    c.configure(w, "https://api.iternio.com:456/1/tlm/send?token=1234-125-r23r2-325&tlm={\"hi\":1234,\"bye\":\"239=\"}");
-    log_v("Set Up Complete");
+    startWiFi();
+
+    setTime();
+
+    char url[50], authkey[50];
+    strcpy(url, abrp::params::PROTOCOL);
+    strcat(url, abrp::params::HOST);
+    strcat(url, abrp::params::SEND_ENDPOINT);
+    strcpy(authkey, abrp::params::HEADER_AUTH_TEXT);
+    strcat(authkey, config::APIKEY);
+    c.configure(w, url);
+    c.urlParams.set(abrp::params::VAR_TOKEN, config::TOKEN);
+    c.reqHeaders.set(abrp::params::HEADER_AUTH, authkey);
+
+    // connectABRP();
+    telem.utc = 1591385576l + millis() / 1000l;
+    telem.soc = randfloat(0, 100, 1);
+    telem.speed = randfloat(0, 160, 1);
+    telem.lat = randfloat(-90, 90, 7);
+    telem.lon = randfloat(-180, 180, 7);
+    telem.is_charging = false;
+    telem.soh = randfloat(0, 100, 1);
+    // sendABRP(telem.toJSON());
+    Serial.println(telem.toJSON());
+
+    c.urlParams.set(abrp::params::VAR_TELEM, telem.toJSON());
+    c.get();
+
+    log_v("Set Up Complete\n\n\n");
+    beep(880, 100, 2);
 }
 
 void loop()
 {
     log_v("Loop");
     unsigned long t = millis();
-    // telem.utc = 1591385576l + millis() / 1000l;
-    // telem.soc = round(telem.soc() + randfloat(-0.5, 0.5, 0.1), 0.1);
-    // telem.speed = round(telem.speed() + randfloat(-5, 5, 0.1), 0.1);
-    // telem.lat = round(telem.lat() + randfloat(-0.1, 0.1, 0.0000001), 0.0000001);
-    // telem.lon = round(telem.lon() + randfloat(-0.1, 0.1, 0.0000001), 0.0000001);
+    telem.utc = 1591385576l + millis() / 1000l;
+    telem.soc = round(telem.soc() + randfloat(-0.5, 0.5, 0.1), 0.1);
+    telem.speed = round(telem.speed() + randfloat(-5, 5, 0.1), 0.1);
+    telem.lat = round(telem.lat() + randfloat(-0.1, 0.1, 0.0000001), 0.0000001);
+    telem.lon = round(telem.lon() + randfloat(-0.1, 0.1, 0.0000001), 0.0000001);
     // sendABRP(telem.toJSON());
-    // Serial.println(telem.toJSON());
+    Serial.println(telem.toJSON());
+
+    c.urlParams.set(abrp::params::VAR_TELEM, telem.toJSON());
+    c.get();
 
 #ifdef VERBOSE
     blink(100);
 #endif
-    if (millis() - t < LOOP_TIME)
-        delay(LOOP_TIME - (millis() - t));
+    if (millis() - t < config::LOOP_TIME)
+        delay(config::LOOP_TIME - (millis() - t));
 }
