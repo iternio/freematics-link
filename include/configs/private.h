a2c83e809215fba8e5ef2c0c3217989540d4d2e2
Billy Gallagher <bjg222@gmail.com>
2020-09-28 20:09:12 -0400

    Update to dummy version of private.h to add new variables

--- include/configs/private.h
+++ include/configs/private.h
@@ -6,8 +6,12 @@
 
 namespace configs {
 
-    inline const char SSID[] = "------------";
-    inline const char PASSWORD[] = "------------";
+    inline const char SSID_SECONDARY[] = "----------";
+    inline const char PASSWORD_SECONDARY[] = "----------";
+    inline const char SSID[] = "----------";
+    inline const char PASSWORD[] = "----------";
+
+    inline const char SIM_APN[] = "----------";
 
     inline const char APIKEY[] = "------------------------------------";
     inline const char TOKEN[] = "------------------------------------";
